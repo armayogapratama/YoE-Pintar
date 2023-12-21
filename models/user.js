213676618a954ac79dbdb6e9ad1cda6f83508194
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2023-12-21 20:25:48 +0700

    Made Register

--- models/user.js
+++ models/user.js
@@ -1,19 +1,33 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       User.hasOne(models.Profile);
     }
+
+    alterPassword() {
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(this.password, salt);
+      return hash;
+    }
   }
-  User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role:DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
+  User.init(
+    {
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+
+  User.beforeCreate((instance) => {
+    instance.password = instance.alterPassword();
   });
 
   return User;
