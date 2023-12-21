ee8966a65fdbb94ae5333c951901a7ee5505d9cb
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2023-12-21 14:48:14 +0700

    Edit fitur done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const router = require("./routes/main");
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, () => {
