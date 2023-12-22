cc6d4b362d6809f7a33ae0f0b4d36e564ceaa4ea
ELvan Surahmat <146643689+ElvanSurahmat@users.noreply.github.com>
2023-12-22 08:29:55 +0700

    elvan

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const { allUser, profile } = require("../controllers/controller");
+const { allUser, profile, addProfile, createProfile } = require("../controllers/controller");
 const { hasloged } = require("../middleware/auth.middleware");
 
 const router = require("express").Router();
@@ -6,5 +6,7 @@ const router = require("express").Router();
 router.use(hasloged);
 router.get("/", allUser);
 router.get("/profile/:id", profile);
+router.get("/profile/add/:id",addProfile)
+router.post("/profile/add/:id",createProfile)
 
 module.exports = router;
